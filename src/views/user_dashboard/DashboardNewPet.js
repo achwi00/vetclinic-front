a1982e59c53a048db606fada630586c451ff0df2
achwi00 <achwierut00@gmail.com>
2024-12-02 15:19:25 +0100

    [Feat] prepared first step of add pet wizard

--- src/views/user_dashboard/DashboardNewPet.js
+++ src/views/user_dashboard/DashboardNewPet.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import SideMenu from "../../component/SideMenu";
+import IconDisplayer from "../../component/IconDisplayer";
 
 function DashboardNewPet(){
+    const [view, setView] = useState("choice");
     const buttons = [
         {label: 'New visit', href:'/dashboard/new-visit'},
         {label: 'My pets', href:'/dashboard/my-pets'},
@@ -14,7 +16,30 @@ function DashboardNewPet(){
             <div className="content-holder">
                 <div className="centring-main">
                     <h2 className="site-tracker">Add new pet</h2>
-
+                    {view ==="choice" &&
+                        <div className="choice-panel-holder">
+                            <div className="choice-panel  choice-shrink">
+                                <div className="choice left" onClick={()=>{setView("pet")}}>
+                                    <h3>Add new pet</h3>
+                                    <IconDisplayer
+                                        iconName="cat"
+                                        styleClass="service-icon"
+                                    />
+                                    <p>Add a new pet.</p>
+                                </div>
+                                <div className="choice right">
+                                    <h3>Add pet group</h3>
+                                    <IconDisplayer
+                                        iconName="group"
+                                        styleClass="service-icon"
+                                    />
+                                    <p>Add a new pet group.</p>
+                                </div>
+                            </div>
+                        </div>
+                    }
+                    {view ==="add-pet"}
+                    {view ==="add-group"}
                 </div>
             </div>
         </div>
