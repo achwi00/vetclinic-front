838043ddec5167d97cf4e8d64b49c5a760680b04
achwi00 <achwierut00@gmail.com>
2024-12-08 18:41:54 +0100

    [Feat] fetching previous visits and displaying

--- src/views/user_dashboard/DashboardVisits.js
+++ src/views/user_dashboard/DashboardVisits.js
@@ -1,14 +1,63 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import SideMenu from "../../component/SideMenu";
 import InnerNav from "../../component/InnerNav";
+import {UserContext} from "../../UserContext";
+import Visit from "../../component/Visit";
+import List from "../../component/List";
 
 function DashboardVisits(){
+    const {user} = useContext(UserContext);
+    const email = user.email;
     const [view, setView] = useState("previous");
-    const handlePrevious = () => {
+    const [prevVisits, setPrevVisits] = useState([]);
+    const [plannedVisits, setPlannedVisits] = useState([]);
+    const handlePrevious = async () => {
         console.log("Previous button clicked");
-        setView("previous");
+
         document.getElementById('prev-vis-btn').style.backgroundColor = '#D99CB0';
         document.getElementById('planned-vis-btn').style.backgroundColor = '#E8C1CE';
+
+        try{
+            const response = await fetch(
+                `http://localhost:8080/visits/completed?email=${email}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const fetchedVisits = await response.json();
+            console.log(fetchedVisits); // Log the visits data
+
+            let tmpArr = [];
+            fetchedVisits.forEach(
+                visit=>{
+                    console.log(visit);
+                    const tmpObj = {
+                        id: visit.id,
+                        date: visit.date,
+                        time: visit.startTime,
+                        vetName: visit.vet.name,
+                        vetSurname: visit.vet.surname,
+                        petName: visit.basePet.name,
+                    }
+                    tmpArr.push(tmpObj);
+                }
+            )
+            console.log(tmpArr);
+            //set visits and change view to visits from form
+            setPrevVisits(tmpArr);
+            setView("previous");
+        }catch(error){
+            console.log(error)
+            console.log("Error fetching visits.")
+        }
     };
 
     const handlePlanned = () => {
@@ -38,7 +87,22 @@ function DashboardVisits(){
                         styleClass="visits-nav"
                         buttonStyleClass="nav-btn"
                     />
-
+                    {view === "previous" && <List
+                        items = {prevVisits.map((visit, index) => (
+                            <Visit
+                                key={index}
+                                id={visit.id}
+                                date={visit.date}
+                                time={visit.time}
+                                vetName={visit.vetName}
+                                vetSurname={visit.vetSurname}
+                                petName={visit.petName}
+                                icon="checkup"
+                                iconClass="vis-icon"
+                            />
+                        ))}
+                        styleClass={"list-holder"}
+                    />}
                 </div>
             </div>
         </div>
