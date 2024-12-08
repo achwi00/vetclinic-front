52d3955be1a020d0c1f52e139286177612780964
achwi00 <achwierut00@gmail.com>
2024-12-08 18:06:42 +0100

    [Feature] Passing nav array to the component, clickable buttons

--- src/component/InnerNav.js
+++ src/component/InnerNav.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const InnerNav = ({ buttons, onButtonClick, styleClass, buttonStyleClass }) => {
+const InnerNav = ({ buttons, styleClass, buttonStyleClass }) => {
     return (
         <nav className={styleClass}>
             {buttons.map((button, index) => (
                 <button
                     key={index}
-                    onClick={() => onButtonClick(button.view)}
+                    onClick={button.onClick}
                     className={buttonStyleClass}
                 >
                     {button.label}
--- src/views/user_dashboard/DashboardVisits.js
+++ src/views/user_dashboard/DashboardVisits.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import SideMenu from "../../component/SideMenu";
 import InnerNav from "../../component/InnerNav";
 
 function DashboardVisits(){
+    const [view, setView] = useState("previous");
+    const handlePrevious = () => {
+        console.log("Previous button clicked");
+        setView("previous");
+    };
+
+    const handlePlanned = () => {
+        console.log("Planned button clicked");
+        setView("planned");
+    };
     const buttons = [
         {label: 'New visit', href:'/dashboard/new-visit'},
         {label: 'My pets', href:'/dashboard/my-pets'},
@@ -10,8 +20,8 @@ function DashboardVisits(){
         {label: 'Visits', href:'/dashboard/visits'},
     ]
     const nav = [
-        {id:'prev-vis-btn', label:'Previous', view:'previous'},
-        {id:'planned-vis-btn',label:'Planned', view:'planned'}
+        {id:'prev-vis-btn', label:'Previous', view:'previous', onClick: handlePrevious},
+        {id:'planned-vis-btn',label:'Planned', view:'planned', onClick: handlePlanned}
     ]
     return(
         <div className="all-holder">
@@ -24,6 +34,7 @@ function DashboardVisits(){
                         styleClass="visits-nav"
                         buttonStyleClass="nav-btn"
                     />
+
                 </div>
             </div>
         </div>
