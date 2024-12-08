d0c9830ad3b75812938352ab2497f7aac91fff6b
achwi00 <achwierut00@gmail.com>
2024-12-08 18:50:02 +0100

    [Feat] Previous and planned visits fetching and displaying

--- src/views/user_dashboard/DashboardVisits.js
+++ src/views/user_dashboard/DashboardVisits.js
@@ -60,11 +60,53 @@ function DashboardVisits(){
         }
     };
 
-    const handlePlanned = () => {
+    const handlePlanned = async () => {
         console.log("Planned button clicked");
-        setView("planned");
+
         document.getElementById('prev-vis-btn').style.backgroundColor = '#E8C1CE';
         document.getElementById('planned-vis-btn').style.backgroundColor = '#D99CB0';
+
+        try{
+            const response = await fetch(
+                `http://localhost:8080/visits/upcoming?email=${email}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const fetchedVisits = await response.json();
+            console.log(fetchedVisits); // Log the visits data
+
+            let tmpArr = [];
+            fetchedVisits.forEach(
+                visit=>{
+                    console.log(visit);
+                    const tmpObj = {
+                        id: visit.id,
+                        date: visit.date,
+                        time: visit.startTime,
+                        vetName: visit.vet.name,
+                        vetSurname: visit.vet.surname,
+                        petName: visit.basePet.name,
+                    }
+                    tmpArr.push(tmpObj);
+                }
+            )
+            console.log(tmpArr);
+            //set visits and change view to visits from form
+            setPlannedVisits(tmpArr);
+            setView("planned");
+        }catch(error){
+            console.log(error)
+            console.log("Error fetching visits.")
+        }
     };
     const buttons = [
         {label: 'New visit', href:'/dashboard/new-visit'},
@@ -89,6 +131,22 @@ function DashboardVisits(){
                     />
                     {view === "previous" && <List
                         items = {prevVisits.map((visit, index) => (
+                            <Visit
+                                key={index}
+                                id={visit.id}
+                                date={visit.date}
+                                time={visit.time}
+                                vetName={visit.vetName}
+                                vetSurname={visit.vetSurname}
+                                petName={visit.petName}
+                                icon="checkup"
+                                iconClass="vis-icon"
+                            />
+                        ))}
+                        styleClass={"list-holder"}
+                    />}
+                    {view === "planned" && <List
+                        items = {plannedVisits.map((visit, index) => (
                             <Visit
                                 key={index}
                                 id={visit.id}
