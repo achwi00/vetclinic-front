36ed94869a9488fa6326a3c9c57727e99786a61a
achwi00 <achwierut00@gmail.com>
2024-12-16 19:21:15 +0100

    [Feat] added detailed visits

---
+++ src/component/DetailedVisit.js
@@ -0,0 +1,107 @@
+import React, {useContext} from "react";
+import IconDisplayer from "./IconDisplayer";
+import {UserContext} from "../UserContext";
+import '../../src/styles/visit.css'
+import '../../src/styles/vetdashboard.css'
+
+function DetailedVisit({id, date, time, vetName, vetSurname, petName, classStyle, icon, iconClass, type, description}) {
+    const {user} = useContext(UserContext);
+    const handleBooking = async () => {
+        console.log("Booking visit...");
+        console.log(user.email)
+        // Implement booking logic here
+        try {
+            const response = await fetch("http://localhost:8080/visits/book-visit", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    visitId: id, // ID of the visit
+                    email: user.email,
+                    petName: petName, // Name of the pet
+
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to book visit: ${response.status}`);
+            }
+
+            const result = response.text();
+            console.log("Visit booked successfully:", result);
+
+            // Optional: Handle UI updates or notifications here
+        } catch (error) {
+            console.error("Error booking visit:", error);
+        }
+
+    };
+
+    // Function to handle canceling the visit
+    const handleCancel = async () => {
+        console.log("Canceling visit...", id);
+        // Implement canceling logic here
+
+        try {
+            const response = await fetch(`http://localhost:8080/visits/cancel-visit?visitId=${id}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to book visit: ${response.status}`);
+            }
+
+            const result = response.text();
+            console.log("Visit cancelled successfully:", result);
+
+            // Optional: Handle UI updates or notifications here
+        } catch (error) {
+            console.error("Error booking visit:", error);
+        }
+    };
+    return(
+        <div className="visit vis-detailed">
+            <div className="date-time-holder detailed">
+                <p>{date} {time}</p>
+            </div>
+            <hr/>
+            <div className="visit-main-holder detailed-holder">
+                <div className="inner-left-visit">
+                    <IconDisplayer
+                        iconName={icon}
+                        styleClass={iconClass}
+                    />
+                    <div className="visit-info-holder">
+                        {type === "completed" &&
+                            <p>{petName}</p>
+                        }
+                        <p>{vetName} {vetSurname}</p>
+                    </div>
+                </div>
+                <div className="inner-right-visit">
+                    {type === "free" && (
+                        <button className="btn book-cancel-btn"
+                                onClick={handleBooking}
+                        >book</button>
+                    )}
+                    {type === "booked" && (
+                        <button className="btn book-cancel-btn"
+                                onClick={handleCancel}
+                        >cancel</button>
+                    )}
+                    {type === "others" &&
+                        <p>{description}</p>
+                    }
+                </div>
+            </div>
+            <div className="more-details">
+                <p>Add details</p>
+            </div>
+        </div>
+    );
+}
+export default DetailedVisit;
--- src/views/vet_dashboard/DashboardIncomingToday.js
+++ src/views/vet_dashboard/DashboardIncomingToday.js
@@ -1,12 +1,23 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import '../../index.css'
 import '../../../src/styles/dashboard.css'
+import '../../../src/styles/vetdashboard.css'
 import SideMenu from "../../component/SideMenu";
 import {UserContext} from "../../UserContext";
+import List from "../../component/List";
+import Visit from "../../component/Visit";
+import DetailedVisit from "../../component/DetailedVisit";
 
 function DashBoardHome(){
     const {user} = useContext(UserContext);
+    const [view, setView] = useState("list");
+    const visits = [
+        {date: '2024-10-12', time:'12:30', petName: 'Cookie', ownerName:'Catherine', ownerSurname:'Smith'},
+        {date: '2024-10-12', time:'12:30', petName: 'Cookie', ownerName:'Catherine', ownerSurname:'Smith'},
+        {date: '2024-10-12', time:'12:30', petName: 'Cookie', ownerName:'Catherine', ownerSurname:'Smith'},
+        {date: '2024-10-12', time:'12:30', petName: 'Cookie', ownerName:'Catherine', ownerSurname:'Smith'},
 
+    ];
     const buttons = [
         {label: 'Incoming today', href:'/dashboard/incoming'},
         {label: 'My schedule', href:'/dashboard/my-schedule'},
@@ -19,7 +30,24 @@ function DashBoardHome(){
             <div className="content-holder">
                 <div className="centring-main">
                     <h2 className="site-tracker">IncomingToday</h2>
-
+                    {view === "list" && <List
+                        items = {visits.map((visit, index) => (
+                            <DetailedVisit
+                                key={index}
+                                id={visit.id}
+                                date={visit.date}
+                                time={visit.time}
+                                vetName={visit.ownerName}
+                                vetSurname={visit.ownerSurname}
+                                petName={visit.petName}
+                                icon="checkup"
+                                iconClass="vis-icon"
+                                type="completed"
+                            />
+                        ))}
+                        styleClass={"list-holder"}
+                        itemsPerPage={4}
+                    />}
                 </div>
             </div>
         </div>
