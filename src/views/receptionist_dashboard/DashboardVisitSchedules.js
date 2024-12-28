0687ab80a828c61f7b7f47610c92b84b6aac0211
achwi00 <achwierut00@gmail.com>
2024-12-28 13:33:04 +0100

    [Feat] fetching visits for vet schedule and displaying

--- src/views/receptionist_dashboard/DashboardVisitSchedules.js
+++ src/views/receptionist_dashboard/DashboardVisitSchedules.js
@@ -1,16 +1,42 @@
 import React, {useContext} from "react";
 import '../../index.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 import '../../../src/styles/dashboard.css'
+import '../../../src/styles/schedules-styles.css'
 import SideMenu from "../../component/SideMenu";
 import {UserContext} from "../../UserContext";
+import enUS from "date-fns/locale/en-US";
+import {Calendar, dateFnsLocalizer} from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
 
 function DashboardVisitSchedules(){
     const {user} = useContext(UserContext);
+
+    const events = [
+        {visitId: 1, date: '2024-12-30',
+            start: new Date(2024, 11, 30, 14, 30),
+            end: new Date(2024, 11, 30, 15, 0), title: 'Eva Smith'}
+    ]
 
     const buttons = [
         {label: 'Schedules', href:'/dashboard/schedules'},
         {label: 'New visit', href:'/dashboard/new-visit/reception'},
     ]
+
+    const locales = {
+        'en-US': enUS,
+    }
+
+    const localizer = dateFnsLocalizer({
+        format,
+        parse,
+        startOfWeek,
+        getDay,
+        locales,
+    })
 
     return(
         <div className="all-holder">
@@ -18,7 +44,13 @@ function DashboardVisitSchedules(){
             <div className="content-holder">
                 <div className="centring-main">
                     <h2 className="site-tracker">Schedules</h2>
-
+                    <Calendar
+                        localizer={localizer}
+                        events={events}
+                        startAccessor="start"
+                        endAccessor="end"
+                        style={{ height: 500 }}
+                    />
                 </div>
             </div>
         </div>
--- src/views/vet_dashboard/DashboardMySchedule.js
+++ src/views/vet_dashboard/DashboardMySchedule.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
 import '../../index.css'
 import SideMenu from "../../component/SideMenu";
@@ -14,6 +14,51 @@ import '../../../src/styles/schedules-styles.css'
 
 function DashBoardHome(){
     const {user} = useContext(UserContext);
+    const email = user.email;
+    const [visits, setVisits] = useState([]);
+    const [view, setView] = useState(null);
+
+    useEffect(() => {
+        const fetchVisits = async () => {
+            try {
+                const response = await fetch(
+                    `http://localhost:8080/visits/vet/all-incoming?email=${email}`,
+                    {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    }
+                );
+
+                if (!response.ok) {
+                    new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                const fetchedVisits = await response.json();
+                console.log("fetched", fetchedVisits)
+                const tempArr = fetchedVisits.map((visit) => ({
+                    id: visit.visitId,
+                    title: visit.title,
+                    start: new Date(visit.start), // Convert ISO string to Date object
+                    end: new Date(visit.end),   // Convert ISO string to Date object
+                }));
+
+                setVisits(tempArr);
+                setView("calendar");
+            } catch (error) {
+                console.error("Error fetching visits:", error);
+            }
+        };
+
+        fetchVisits();
+
+    }, [email]);
+
+    useEffect(() => {
+        console.log("Updated Visits:", visits);
+    }, [visits]); // Runs whenever visits are updated
+
     const events = [
         {visitId: 1, date: '2024-12-30',
             start: new Date(2024, 11, 30, 14, 30),
@@ -41,15 +86,17 @@ function DashBoardHome(){
             <div className="content-holder">
                 <div className="centring-main">
                     <h2 className="site-tracker">My schedule</h2>
-                    <div className="calendar-holder">
-                        <Calendar
-                            localizer={localizer}
-                            events={events}
-                            startAccessor="start"
-                            endAccessor="end"
-                            style={{ height: 500 }}
-                        />
-                    </div>
+                    {view === "calendar" &&
+                        <div className="calendar-holder">
+                            <Calendar
+                                localizer={localizer}
+                                events={visits}
+                                startAccessor="start"
+                                endAccessor="end"
+                                style={{ height: 500 }}
+                            />
+                        </div>
+                    }
                 </div>
             </div>
         </div>
